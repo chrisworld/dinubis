4bc3777b5d10b3204124348dc7df71235273048c
Christian <christian.walter@outlook.at>
2019-01-08 00:25:41 +0100

    Resident Dictionary added

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # dinubis
 
-by Christian Walter, Jürgen Mayer, Andreas Greindl, Brian Questa
+by Christian Walter, Jürgen Mayer, Andreas Greindl and Brian Questa
 
 Computermusik and Multimedia Seminar 01
 
+used Unity Version: 2018.2.19f1
 ----------------------------------------------------------------
 
--- dinubis_unity/Assets/Scripts/Resident.cs
+++ dinubis_unity/Assets/Scripts/Resident.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -16,31 +17,48 @@ public class Resident : MonoBehaviour {
   void Start () {
     agent = gameObject.GetComponent<NavMeshAgent>();
     agent.SetDestination(gameObject.GetComponent<Transform>().position);
+
+    OSC myOsc = GameObject.Find ("OSCManager").GetComponent<OSC> ();;
+    OscMessage msg = new OscMessage ();
+    msg.address = "/soundObject";
+    msg.values.Add (transform.position.x);
+    msg.values.Add (transform.position.y);
+    msg.values.Add (transform.position.z);
+    myOsc.Send (msg);
+    Debug.Log("Send message /soundObject");
   }
   
   // Update is called once per frame
-  void Update () {
+  void Update () 
+  {
+    CalculateDistances();
+  }
+
+  // calculate Distances
+  private void CalculateDistances()
+  {
     // calculate distances
     Vector3 resident_pos = gameObject.GetComponent<Transform>().position;
     nubis = GameObject.FindGameObjectsWithTag("Player");
+
+    //List<float> distance_list = new List<float>();
+    Dictionary<GameObject, float> distance_dict = new Dictionary<GameObject, float>();
     foreach (GameObject nubi in nubis)
     {
       // TODO: calculate distances
-      //resident_dist = Vector3.Distance(resident_pos, nubi.position);
+      float distance = (resident_pos - nubi.transform.position).sqrMagnitude;
+      distance_dict.Add(nubi, distance);
     }
 
-    // calculate distances to each player
-    //float resident_dist = Vector3.Distance(wolf_pos, shepherd.position);
-
-    //Debug.Log("distance: " + shepherd_dist + white_sheep_dist);
-    // follow closest object
-    /*
-    if (white_sheep_dist < follow_dist){
-      agent.SetDestination(white_sheep.position);
-      follow_shepherd = false;
-      follow_white_sheep = true;
+    // follow closest nubi
+    if (distance_dict.Count != 0){
+      //float max = distance_dict.Values.Max();
+      float min = distance_dict.Values.Min();
+      //Debug.Log("max/min distance: " + max + " / " + min);
+      if (min < follow_dist * follow_dist){
+        //agent.SetDestination(white_sheep.position);
+      }
     }
-    */
   }
 
   // Collision
