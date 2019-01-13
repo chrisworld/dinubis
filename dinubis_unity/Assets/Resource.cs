e12be6a02afc71fc16baeaa5fe6a56178253487a
andiausriad <andreas.greindl@gmx.at>
2019-01-13 18:53:39 +0100

    Resource TakeDamage script
    Skizze des Scripts.
    Dieses Script leitet das Ende des Spiels ein.

---
+++ dinubis_unity/Assets/Resource.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Resource : MonoBehaviour {
+
+    public float health = 100;
+
+    [Header("Unity")]
+    public Image healthBar;
+
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+
+        healthBar.fillAmount = health / 100f;
+
+        if (health <= 0)
+        {
+            End();
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
