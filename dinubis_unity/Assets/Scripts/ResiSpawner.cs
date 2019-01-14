199d4edd65dcade257161e483f2efe37ebdc3396
chrisworld <christian.walter@outlook.at>
2019-01-14 16:06:20 +0100

    resi spawner

---
+++ dinubis_unity/Assets/Scripts/ResiSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ResiSpawner : NetworkBehaviour {
+
+  public Transform spawn_center;
+  public GameObject resi_prefab;
+  public int n_resi = 1;
+
+  // Use this for initialization
+  public override void OnStartServer () // This is invoked for NetworkBehaviour objects when they become active on the server.
+  {
+    for (int index = 0; index < n_resi; index = index + 1) 
+    {
+      Vector3 spawnPosition = new Vector3 (spawn_center.position.x + Random.Range(-20f, 20f), 0.5f, spawn_center.position.z + Random.Range(-20f, 20f));
+      //Vector3 spawnPosition = new Vector3 (104f, 0.5f, 43f);
+      Quaternion spawnRotation = Quaternion.Euler (0f, 0f, 0f);
+
+      GameObject resi = (GameObject)Instantiate (resi_prefab, spawnPosition, spawnRotation);
+      NetworkServer.Spawn (resi);
+    }
+  }
+}
